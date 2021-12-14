f901e41ea2b6ce3d0869ff347b81dbc04167b58e
beny996 <94617004+beny996@users.noreply.github.com>
2021-12-14 17:35:44 +0100

    Cetvrta nedelja, utorak

--- 13_FUNKCIJE/script.js
+++ 13_FUNKCIJE/script.js
@@ -113,36 +113,186 @@ kolicnikDvaBroja(25, 5);
 
 //Zadatak 3.
 
-function neparan(n){
-    if(n % 2 != 0){
-        let tacno = "Tacno";
-        return tacno;
-    }
-    else{
-        let netacno = "Netacno";
-        return netacno;
-    }
-}
+// function neparan(n){
+//     if(n % 2 != 0){
+//         let tacno = "Tacno";
+//         return tacno;
+//     }
+//     else{
+//         let netacno = "Netacno";
+//         return netacno;
+//     }
+// }
 
-let rezultat = neparan(13);
-console.log(rezultat);
+// let rezultat = neparan(13);
+// console.log(rezultat);
 
 //Zadatak 4.
 
 function maks2(a, b){
     if(a > b){
-        let maks = a;
-        return maks;
+        return a;
+    }
+    else if(b > a){
+        return b;
     }
     else{
-        let maks = b;
-        return maks;
+        return "Brojevi su jednaki"
     }
 }
 
-let rezultat2 = maks2(5, 3);
+let rezultat2 = maks2(5, 5);
 console.log(rezultat2);
 
 function maks4(a, b, c, d){
-    
+    if(a > b && a > c && a > d){
+        return a;
+    }
+    else if(b > a && b > c && b > d){
+        return b;
+    }
+    else if(c > a && c > b && c > d){
+        return c;
+    }
+    else if(d > a && d > b && d > c){
+        return d;
+    }
+    else{
+        return "Pogresan unos, ili su brojevi jednaki"
+    }
 }
+
+let rezultat3 = maks4(1, 3, 7, 4);
+console.log(`Najveci broj je ${rezultat3}`);
+      
+    
+
+
+// let rezultat3 = maks4(3, 4, 7, 1);
+// console.log(rezultat3);
+
+////////////////////////////////////////////
+
+//Zadatak 2. (Jelena) Napisati funkciju zbir koja računa zbir dva realna broja.  
+//Šta bi trebalo izmeniti da bi se dobila razlika, proizvod ili količnik dva broja.
+
+function racunaj(br1, br2, operacija){
+    if(operacija == "+"){
+        let rezultat = br1 + br2;
+        console.log(rezultat);
+    }
+    else if(operacija == "-"){
+        let rezultat = br1 - br2;
+        console.log(rezultat);
+    }
+    else if(operacija == "*"){
+        let rezultat = br1 * br2;
+        console.log(rezultat);
+    }
+    else if(operacija == "/"){
+        if(br2 == 0){
+            console.log("Deljenje nulom nije dozvoljeno");
+        }
+        else{
+            let rezultat = br1 / br2;
+            console.log(rezultat);
+        }
+    }
+    else{
+        console.log("Pogresan unos");
+    }
+}
+
+racunaj(5, 4, "+");
+racunaj(5, 4, "-");
+racunaj(5, 4, "*");
+racunaj(5, 4, "/");
+racunaj(5, 4, "%");
+racunaj(5, 0, "/");
+
+//Zadatak 3. (Jelena)
+//Napisati funkciju neparan koja za uneti ceo broj n vraća tačno ukoliko je neparan ili netačno ukoliko nije neparan.
+
+function neparan(n){
+    if(n % 2 != 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let nep = neparan(3);
+console.log(nep);
+
+function neparan1(n){
+    let rez = true;
+    if(n % 2 == 0){
+        rez = false;
+    }
+    return rez;
+}
+
+let nep1 = neparan1(4);
+console.log(nep1);
+
+function neparan2(n){
+    return n % 2 != 0;
+}
+
+let nep2 = neparan2(164);
+console.log(nep2);
+
+//Zadatak 5.
+
+function slika(adresa){
+    document.body.innerHTML += `<img src = "${adresa}">`
+}
+
+slika("https://www.oxfordwebstudio.com/user/pages/06.da-li-znate/sta-je-html/sta-je-html.jpg");
+
+//Zadatak 6.
+
+function paragraf(boja){
+    document.body.innerHTML += `<p style = "color: ${boja}">Tekst koji se boji</p>`
+}
+
+paragraf("blue");
+
+//Zadatak 7.
+
+function sedmiDan(n){
+
+}
+
+//Zadatak 8.
+
+function deljivSaTri(n, m){
+    let br = 0;
+    for(let i = n; i <= m; i++){
+        if(i % 3 == 0){
+            console.log(i);
+            br++;
+        }
+    }
+    console.log(`Ima ${br} brojeva deljivih sa 3 u intervalu od ${n} do ${m}`);
+}
+
+deljivSaTri(2, 13);
+
+//Zadatak 15.
+
+/*Dobili ste plaćenu programersku praksu u trajanju od n meseci. Prvog meseca, plata će biti a dinara, a ako budete vredno radili svakog narednog meseca ćete dobiti povišicu od d dinara. Brojeve n, a i d određujete sami.
+ Napišite funkciju kojoj se prosleđuju brojevi n, a i d. Funkcija treba da vrati vrednost koliko ćete ukupno navca zaraditi, ukoliko svakog meseca budete dobijali povišicu.
+ Testirati zadatak (pozvati funkciju i ispisati vrednost koju ona vraća).*/
+
+
+function plata(n, a, d){
+    for(i = 2; i <= n; i++){
+        a += d;
+    }
+    return a;
+}
+
+let plata2 = plata(5, 500, 100);
+console.log(plata2);

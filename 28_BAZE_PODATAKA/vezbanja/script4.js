7f113f6ae9c0b536277eb5e7173755a5971b977a
beny996 <94617004+beny996@users.noreply.github.com>
2022-01-28 16:53:37 +0100

    Deveta nedelja, petak

--- 28_BAZE_PODATAKA/vezbanja/script4.js
+++ 28_BAZE_PODATAKA/vezbanja/script4.js
@@ -191,9 +191,16 @@
 // });
 
 //Zadatak 3.
+// let datum = new Date();
 
+// let year = datum.getFullYear();
+// let date1 = new Date(year, 0, 1); // 1. januar tekuce godine
+// let date2 = new Date(year + 1, 0, 1); // 1. januar sledece godine
+// let date1Timestamp = firebase.firestore.Timestamp.fromDate(date1);
+// let date2Timestamp = firebase.firestore.Timestamp.fromDate(date2);
 // db.collection("tasks")
-// .where("due_date", "")
+// .where("due_date", ">=", date1Timestamp)
+// .where("due_date", "<", date2Timestamp)
 // .get()
 // .then(snapshot => {
 //     if(!snapshot.empty){
@@ -210,11 +217,12 @@
 //     console.log(`Desila se neka greska ${err}`);
 // });
 
-//Zadatak 4.
-let datum = new Date();
+// //Zadatak 4.
+// let datumTimestamp = firebase.firestore.Timestamp.fromDate(datum);
+
 
 // db.collection("tasks")
-// .where("due_date", "<", datum)
+// .where("due_date", "<=", datumTimestamp)
 // .get()
 // .then(snapshot => {
 //     if(!snapshot.empty){
@@ -233,8 +241,137 @@ let datum = new Date();
 
 //Zadatak 5.
 
-db.collection("tasks")
-.where("start_date", ">", datum)
+
+// db.collection("tasks")
+// .where("start_date", ">", datumTimestamp)
+// .get()
+// .then(snapshot => {
+//     if(!snapshot.empty){
+//         let allDocs = snapshot.docs;
+//         allDocs.forEach(d => {
+//             console.log(d.data());
+//         });
+//     }
+//     else{
+//         console.log(`Nema zadataka sa zadatim uslovima`);
+//     }
+// })
+// .catch(err => {
+//     console.log(`Desila se neka greska ${err}`);
+// });
+
+//Filmovi
+
+//Zadatak 1.
+
+// db.collection("movies")
+// .where("director.name", "==", "Jon")
+// .where("director.surname", "==", "Watts")
+// .get()
+// .then(snapshot => {
+//     if(!snapshot.empty){
+//         let allDocs = snapshot.docs;
+//         allDocs.forEach(d => {
+//             console.log(d.data());
+//         });
+//     }
+//     else{
+//         console.log(`Nema zadataka sa zadatim uslovima`);
+//     }
+// })
+// .catch(err => {
+//     console.log(`Desila se neka greska ${err}`);
+// });
+
+//Zadatak 2.
+
+// db.collection("movies")
+// .where("rating", ">=", 8)
+// .where("rating", "<=", 9)
+// .get()
+// .then(snapshot => {
+//     if(!snapshot.empty){
+//         let allDocs = snapshot.docs;
+//         allDocs.forEach(d => {
+//             console.log(d.data());
+//         });
+//     }
+//     else{
+//         console.log(`Nema zadataka sa zadatim uslovima`);
+//     }
+// })
+// .catch(err => {
+//     console.log(`Desila se neka greska ${err}`);
+// });
+
+//Zadatak 3.
+
+// db.collection("movies")
+// .where("genres", "array-contains-any", ["comedy", "tragedy", "drama"])
+// .get()
+// .then(snapshot => {
+//     if(!snapshot.empty){
+//         let allDocs = snapshot.docs;
+//         allDocs.forEach(d => {
+//             console.log(d.data());
+//         });
+//     }
+//     else{
+//         console.log(`Nema zadataka sa zadatim uslovima`);
+//     }
+// })
+// .catch(err => {
+//     console.log(`Desila se neka greska ${err}`);
+// });
+
+//Zadatak 4.
+
+// db.collection("movies")
+// .where("release_year", ">=", 2001)
+// .where("release_year", "<=", 2100)
+// .get()
+// .then(snapshot => {
+//     if(!snapshot.empty){
+//         let allDocs = snapshot.docs;
+//         allDocs.forEach(d => {
+//             console.log(d.data());
+//         });
+//     }
+//     else{
+//         console.log(`Nema zadataka sa zadatim uslovima`);
+//     }
+// })
+// .catch(err => {
+//     console.log(`Desila se neka greska ${err}`);
+// });
+
+//Zadatak 5.
+
+// db.collection("movies")
+// .orderBy("rating", "desc")
+// .limit(1)
+// .get()
+// .then(snapshot => {
+//     if(!snapshot.empty){
+//         let allDocs = snapshot.docs;
+//         allDocs.forEach(d => {
+//             console.log(d.data());
+//         });
+//     }
+//     else{
+//         console.log(`Nema zadataka sa zadatim uslovima`);
+//     }
+// })
+// .catch(err => {
+//     console.log(`Desila se neka greska ${err}`);
+// });
+
+//Zadatak 6.
+
+db.collection("movies")
+.where("genres", "array-contains", "comedy")
+.orderBy("rating", "asc")
+.limit(1)
 .get()
 .then(snapshot => {
     if(!snapshot.empty){
